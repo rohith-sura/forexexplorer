62c3e165c1704f8b2e9e7ca1a9541ca03a7df896
rohith-sura <surarohith97@gmail.com>
2024-02-06 18:15:13 -0500

    Added controller response

--- src/main/java/com/forexexplorer/controller/ExchangeController.java
+++ src/main/java/com/forexexplorer/controller/ExchangeController.java
@@ -1,5 +1,6 @@
 package com.forexexplorer.controller;
 
+import com.forexexplorer.model.CurrencyConverterResponse;
 import com.forexexplorer.service.CurrencyConverterService;
 import lombok.AllArgsConstructor;
 import org.apache.hc.core5.http.ParseException;
@@ -11,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.net.URISyntaxException;
-import java.time.LocalDate;
 
 @RestController
 @AllArgsConstructor
@@ -20,13 +20,12 @@ public class ExchangeController {
     private final CurrencyConverterService currencyConverterService;
 
     @GetMapping(value = "/currency-converter")
-    public ResponseEntity<String> currencyConverter(
-            @RequestParam(value = "date") LocalDate date,
+    public ResponseEntity<CurrencyConverterResponse> currencyConverter(
             @RequestParam(value = "fromCurrency") String fromCurrency,
             @RequestParam(value = "toCurrency") String toCurrency,
             @RequestParam(value = "amount") BigDecimal amount) throws URISyntaxException, IOException, ParseException {
 
-        return ResponseEntity.ok(currencyConverterService.currencyConverter(date, fromCurrency, toCurrency, amount));
+        return ResponseEntity.ok(currencyConverterService.currencyConverter(fromCurrency, toCurrency, amount));
     }
 
 }
--- src/main/java/com/forexexplorer/model/CurrencyConverterResponse.java
+++ src/main/java/com/forexexplorer/model/CurrencyConverterResponse.java
@@ -1,22 +1,20 @@
 package com.forexexplorer.model;
 
+import lombok.Builder;
 import lombok.Data;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Data
+@Builder
 public class CurrencyConverterResponse {
 
-    private LocalDate fromDate;
-    private LocalDate toDate;
-    private String fromCurrency = "";
+    private String fromCurrency;
     private String toCurrency;
-    private String statusMessage;
-    private String statusCode;
-    private BigDecimal convertedAmount;
+    private BigDecimal amount;
     private BigDecimal rate;
-    private LocalDateTime timestamp;
+    private BigDecimal convertedAmount;
+    private LocalDateTime dateTime;
 
 }
